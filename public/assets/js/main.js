1c349ad3a2d9260cbb06674ca3f39e2da3b73cb2
FranklinIvan <franklin.alvarado@utp.ac.pa>
2021-06-20 23:07:22 -0500

    Mejoras del darkmode

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -113,4 +113,21 @@ const btnSwitch = document.querySelector('.switch');
 btnSwitch.addEventListener('click',()=>{
     document.body.classList.toggle('dark-mode');
     btnSwitch.classList.toggle('active-mode');
+
+    // Localstorage
+    if(document.body.classList.contains('dark-mode')){
+        localStorage.setItem('dark-mode','true');
+    }else{
+        localStorage.setItem('dark-mode','false');
+    }
 });
+
+
+// Localstorage mode
+if(localStorage.getItem('dark-mode') === 'true'){
+    document.body.classList.add('dark-mode');
+    btnSwitch.classList.add('active-mode');
+}else{
+    document.body.classList.remove('dark-mode');
+    btnSwitch.classList.remove('active-mode');
+}
