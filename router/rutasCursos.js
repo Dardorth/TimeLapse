8746af266f5abfc0f02e4fde9f81301c507604ea
Javier Arrue <armandoutp1@gmail.com>
2021-07-04 16:23:47 -0500

    Autenticacion de nuevo xD

--- router/rutasCursos.js
+++ router/rutasCursos.js
@@ -2,15 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 // Rutas para cursos
-router.get('/curso-whatsapp', (req, res) => {
+router.get('/curso-whatsapp', isAuthenticated, (req, res) => {
     res.render('courses/whatsapp/seccion1');
 });
-router.get('/curso-whatsapp-p1', (req, res) => {
+router.get('/curso-whatsapp-p1', isAuthenticated, (req, res) => {
     res.render('courses/whatsapp/prueba1');
 });
 
-router.get('/prueba', (req, res) => {
+router.get('/prueba', isAuthenticated, (req, res) => {
     res.render('pages/prueba');
 });
+
+function isAuthenticated(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/');
+}
 
 module.exports = router;
