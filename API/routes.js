d73207d74bf7a185fd425b922d8cebe921e0baa7
Javier Arrue <armandoutp1@gmail.com>
2021-07-11 13:32:01 -0500

    Ajustando un poco el API

--- API/routes.js
+++ API/routes.js
@@ -7,15 +7,10 @@ const Users = require('../models/user');
 router.get('/API/cursos', async (req, res) => {
     
     try {
-        //LLAMO A TODOS LOS CURSOS SIN FILTRAR
-        const administrarCursos = await Product.find({}).select({ 
-            "name": 1,
-            "price": 1,
-            "_id": 0
-        });
-
-        //ENVIO LA INFORMACION DE LOS CURSOS A administrarCursos
-            res.send({administrarCursos});
+        const cursos = await Product.find({});
+        console.log(cursos[0].price)
+        
+            res.send({cursos});
         } catch (err) {
             console.log(err);
         }
@@ -26,14 +21,14 @@ router.get('/API/cursos', async (req, res) => {
 router.get('/API/usuarios', async (req, res) => {
     
     try {
-        //LLAMO A TODOS LOS CURSOS SIN FILTRAR
-        const usuarios = await Users.find({}).select({ 
+        
+        const usuarios = await Users.find({role: "client"}).select({ 
             "name": 1,
             "lastname":1,
             "_id": 0
         });
 
-        //ENVIO LA INFORMACION DE LOS CURSOS A administrarCursos
+        
             res.send({usuarios});
         } catch (err) {
             console.log(err);
